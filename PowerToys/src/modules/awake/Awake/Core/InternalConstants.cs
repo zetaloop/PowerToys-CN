734a36d60ca946b83f202fdc2cb2c77c87c4014d
Zetaspace <ideaploter@outlook.com>
2021-08-07 01:21:54 +0800

    PowerToys v0.43.0 Translation v10.1 Incremental Pack
    增量包，请下载官方源码后覆盖

---
+++ PowerToys/src/modules/awake/Awake/Core/InternalConstants.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Awake.Core
+{
+    internal static class InternalConstants
+    {
+        internal const string AppName = "Awake";
+        internal const string FullAppName = "唤醒工具"; // 这个 FullAppName 好像也只有悬停显示才用上... //internal const string FullAppName = "PowerToys " + AppName;
+    }
+}
